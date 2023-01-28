8f156be670c61b264e7ef48d095d121948fb798c
kevinnail <fumalicious@gmail.com>
2023-01-28 13:13:19 -0800

    from/ setFrom in context in exexutePassButton.js as well

--- src/components/ExecutePassButton.js
+++ src/components/ExecutePassButton.js
@@ -2,11 +2,11 @@ import React, { useContext } from 'react';
 import { GameContext } from '../GameContext.js';
 import Card from './Card';
 
-export default function ExecutePassButton({ passCard, setFrom, from }) {
+export default function ExecutePassButton({ passCard, from }) {
   const { selectedCard, to } = useContext(GameContext);
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
-      Pass <Card card={selectedCard} setFrom={setFrom} /> from {from} to {to}
+      Pass <Card card={selectedCard} /> from {from} to {to}
     </div>
   );
 }
