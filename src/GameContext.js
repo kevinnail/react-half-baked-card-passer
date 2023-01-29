ff33095ba9c2af634d55059f058d1d291c46bc06
kevinnail <fumalicious@gmail.com>
2023-01-28 16:04:19 -0800

    moved passCard into ExecutePassButton, moved player hands and deck into context

--- src/GameContext.js
+++ src/GameContext.js
@@ -1,14 +1,36 @@
 import { useState } from 'react';
 import { createContext } from 'react';
-
+import initialCards from './cards-data.js';
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [selectedCard, setSelectedCard] = useState(null);
   const [to, setTo] = useState(1);
   const [from, setFrom] = useState('deck');
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [deck, setDeck] = useState(initialCards);
+
   return (
-    <GameContext.Provider value={{ selectedCard, setSelectedCard, to, setTo, from, setFrom }}>
+    <GameContext.Provider
+      value={{
+        selectedCard,
+        setSelectedCard,
+        to,
+        setTo,
+        from,
+        setFrom,
+        playerOneHand,
+        setPlayerOneHand,
+        playerTwoHand,
+        setPlayerTwoHand,
+        playerThreeHand,
+        setPlayerThreeHand,
+        deck,
+        setDeck,
+      }}
+    >
       {children}
     </GameContext.Provider>
   );
